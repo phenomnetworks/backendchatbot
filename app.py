b4223c41618adf9e829df70be2db6cb42fa1f866
phenomnetworks <91694599+phenomnetworks@users.noreply.github.com>
2022-06-29 23:50:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,17 @@
-from flask import Flask, jsonify, request
-import json
-
-response = ''
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+from chat import get_response
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/name', methods = ['GET', 'POST'])
-def nameRoute():
+@app.post('/')
+def predict():
+    text = request.get_json().get("message")
+    # TODO: check if text is valid
+    response = get_response(text)
+    message = {"answer": response}
+    return jsonify(message)
 
-    global response
-
-    if(request.method == 'POST'):
-        request_data = request.data
-        request_data = json.loads(request_data.decode('utf-8'))
-        name = request_data['name']
-        response = f'{name}'
-        return " "
-
-    else:
-        return jsonify({'name' : response})   
-
-@app.route('/name2', methods = ['GET'])
-def nameRoute2():
-
-    global response
-
-    response2 = f'Hello {response}!'
-
-    return jsonify({'name2' : response2})
-    
 if __name__ == "__main__":
     app.run(debug=True)
