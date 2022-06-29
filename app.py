3e71dde40d2a5210dde2a68135137d86d919246d
phenomnetworks <91694599+phenomnetworks@users.noreply.github.com>
2022-06-29 20:03:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from chat import get_response
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/predict', methods = ['POST'])
+@app.post('/predict')
 def predict():
     text = request.get_json().get("message")
     # TODO: check if text is valid
